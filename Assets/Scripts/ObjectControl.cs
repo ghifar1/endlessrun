a5751c41495fe037888a975f0586fa76a952d76c
muhammad al ghifari <muhamadalghifari97@gmail.com>
2022-03-15 22:02:46 +0700

    first commit

---
+++ Assets/Scripts/ObjectControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectControl : MonoBehaviour
+{
+    public Transform pool;
+
+    private void OnEnable()
+    {
+        for (int i = 0; i < pool.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(true);
+        }
+    }
+}
